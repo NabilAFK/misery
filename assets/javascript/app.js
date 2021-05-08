2308b68a4d88beccb1537730f3910aa635853162
Alex <74885892+misspoken69@users.noreply.github.com>
2021-05-08 11:52:37 -0700

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,7 +8,7 @@ class _app {
   backgroundToggler = false;
   shouldIgnoreVideo = false;
   effects = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'swing', 'tada', 'wobble', 'jello'];
-  brandDescription = ['developer', 'producer', 'youtuber', 'packer', 'programmer', 'on top'];
+  brandDescription = ['accounts', 'discord hacks', 'developers', 'youtubers', 'tools', 'on top'];
 
   titleChanger = (text, delay) => {
     if (!text) return;
